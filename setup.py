09fdf56d8aa5124b71db73bf334bf287e6e78316
OneBST <onebst@foxmail.com>
2022-07-02 18:42:41 +0800

    v0.2.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ URL = 'https://github.com/OneBST/GGanalysisLite'
 EMAIL = 'onebst@foxmail.com'
 AUTHOR = 'OneBST'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.2.1'
+VERSION = '0.2.2'
 REQUIRED = [
     'numpy', 'scipy', 'matplotlib'
 ]
