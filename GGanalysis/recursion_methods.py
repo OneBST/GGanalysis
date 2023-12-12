15f107f53b050bdb303fd5c23e8055d2635caa15
OneBST <onebst@foxmail.com>
2023-12-12 23:04:29 +0800

    add scored item mix dist source rate

--- GGanalysis/recursion_methods.py
+++ GGanalysis/recursion_methods.py
@@ -8,8 +8,9 @@ class GeneralCouponCollection():
         不同奖券概率不均等的奖券集齐问题类
         默认以集齐为吸收态，状态按照从低到高位对应输入列表的从前往后
     '''
-    def __init__(self, p_list: Union[list, np.ndarray], item_name: list[str]=None) -> None:
+    def __init__(self, p_list: Union[list, np.ndarray], item_names: list[str]=None) -> None:
         # 输入合法性检查
+        # TODO 考虑是否添加多个吸收态的功能，还是出于速度考虑用DP代替这部分功能
         if len(p_list) >= 16:
             # 默认最高道具种数为 16 种，以防状态过多
             raise ValueError("Item types must below 16")
@@ -20,16 +21,16 @@ class GeneralCouponCollection():
             sum_p += p
         if sum_p > 1:
             raise ValueError("Sum P is greater than 1!")
-        if item_name is not None:
-            if len(item_name) != len(p_list):
+        if item_names is not None:
+            if len(item_names) != len(p_list):
                 raise ValueError("item_name must have equal len with p_list!")
         # 类赋值
         self.p_list = np.array(p_list)  # 目标物品的概率，其概率和可以小于1
-        self.item_name = item_name  # 道具名称 考虑用字典存其编号
-        if item_name is not None:
+        self.item_names = item_names  # 道具名称 考虑用字典存其编号
+        if item_names is not None:
             # 建立编号查找表，按照列表中的顺序进行编号
             self.item_dict = {}
-            for i, item in enumerate(self.item_name):
+            for i, item in enumerate(self.item_names):
                 self.item_dict[item] = i
         self.fail_p = 1 - sum(p_list)  # 不是目标中的任意一个的概率
         self.item_types = len(p_list)
@@ -56,7 +57,7 @@ class GeneralCouponCollection():
         ans = []
         for i in range(self.item_types):
             if state_num & (1 << i):
-                ans.append(self.item_name[i])
+                ans.append(self.item_names[i])
         return ans
 
     def get_satisfying_state(self, target_state=None):
