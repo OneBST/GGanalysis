c45e760e467e3032226391b1cdd1bd10ed5a65b3
OneBST <onebst@foxmail.com>
2025-04-01 20:36:39 +0800

    更新终末地二测模型猜测

--- GGanalysis/games/arknights_endfield/gacha_model.py
+++ GGanalysis/games/arknights_endfield/gacha_model.py
@@ -4,16 +4,22 @@ from GGanalysis.basic_models import *
 
 '''
     注意，本模块按明日方舟：终末地二测公示进行简单建模，非公测版本
+    武器池由于描述不够清晰，采用猜测机制
 '''
 
 __all__ = [
     'PITY_6STAR',
     'PITY_5STAR',
+    'PITY_W6STAR',
+    'PITY_W5STAR',
     'common_6star',
+    'weapon_6star',
     'up_6star_character',
     'common_5star',
+    'weapon_5star',
 ]
 class AKESinglePityModel(GachaModel):
+    '''终末地二测硬保底模型'''
     def __init__(self, pity_p, up_p, up_pity_pull):
         super().__init__()
         self.pity_p = pity_p
@@ -64,14 +70,27 @@ PITY_6STAR[80] = 1
 PITY_5STAR = np.zeros(11)
 PITY_5STAR[1:9+1] = 0.08
 PITY_5STAR[10] = 1
+# 设置武器池6星概率递增表
+PITY_W6STAR = np.zeros(41)
+PITY_W6STAR[1:39+1] = 0.04
+PITY_W6STAR[40] = 1
+# 设置武器池5星概率递增表
+PITY_W5STAR = np.zeros(11)
+PITY_W5STAR[1:9+1] = 0.15
+PITY_W5STAR[10] = 1
 
 # ★★★★★★
 common_6star = PityModel(PITY_6STAR)
 up_6star_character_after_first = PityBernoulliModel(PITY_6STAR, 0.5)  # 不考虑第一个
 up_6star_character = AKESinglePityModel(PITY_6STAR, 0.5, 120)
+weapon_6star = PityModel(PITY_W6STAR)
+up_weapon_6star_first = DualPityModel(PITY_W6STAR, [0, 0.25, 1])  # 每卡池的首个UP武器
+up_weapon_6star_after_first = PityBernoulliModel(PITY_W6STAR, 0.25)  # 每卡池的首个UP武器
+up_6star_weapon = AKESinglePityModel(PITY_W6STAR, 0.25, 80)
 # ★★★★★
 # 五星公示基础概率为8%，更多信息还有待发掘，目前看来获取6星会重置5星保底
 common_5star = PityModel(PITY_5STAR)  # 不考虑被6星重置的简单模型
+weapon_5star = PityModel(PITY_W5STAR)  # 不考虑被6星重置的简单模型
 up_5star_character = PityBernoulliModel(PITY_5STAR, 0.5)  # 不考虑被6星重置的简单模型
     
 if __name__ == '__main__':
